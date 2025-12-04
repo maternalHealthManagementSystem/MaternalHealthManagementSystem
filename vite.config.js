0156c4800616d7000c63350591890314e25c6204
zhiz205 <lisa2005050907@gmail.com>
2025-12-05 00:11:33 +0800

    Update repository name

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const repoName = 'MaternalHealthManagementSystem'; 
+// const repoName = 'MaternalHealthManagementSystem'; 
 
 export default defineConfig({
-  base: `/${repoName}/`, // <-- 新增此行
-  plugins: [
-    vue(),
-  ],
+  // base: `/${repoName}/`, // <-- 新增此行
+  // plugins: [
+  //   vue(),
+  // ],
+  plugins: [vue()],
+  base: '/MaternalHealthManagementSystem/',
   // resolve: {
   //   alias: {
   //     // 設置 '@' 別名，指向專案的 'src' 資料夾
