369d6682d05bb2b81c4d8f97dafc79dfdfdedac5
minsyun <nana.hsiao0914@gmail.com>
2025-12-04 21:00:36 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import login from "../views/login.vue";
 import home from "../views/home.vue";
 import profile from "../views/profile.vue";
@@ -77,7 +77,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/MaternalHealthManagementSystem'),
+  history: createWebHashHistory('/MaternalHealthManagementSystem'),
   routes,
 });
 
