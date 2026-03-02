8a3f37cd7b3badae136cbf63797eff785a4328dd
chiehen <qoqo111866@gmail.com>
2026-03-02 12:56:52 +0800

    20260302-1256-home.vue修改

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="maternal-dashboard">
     <div class="main-content-container">
@@ -109,7 +108,7 @@ const combinedCalendarData = computed(() => {
 });
 
 onMounted(async () => {
-  // await calendarStore.fetchAllData('U001');
+  await calendarStore.fetchAllData('U001');
 
   if (router.query.date) {
     newDiary.value.date = router.query.date;
