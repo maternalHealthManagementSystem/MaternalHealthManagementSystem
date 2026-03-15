376c82b04a3d1dafd29e4dfaed694c9adec2bd4f
chiehen <qoqo111866@gmail.com>
2026-03-15 11:05:49 +0800

    20260315-1104-更新JWT機制

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,16 @@ const api = axios.create({
   timeout: 5000,
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
 
 export default api;
