a56a9075974065ef84607d5aae2c2c9310abd948
chiehen <qoqo111866@gmail.com>
2026-04-21 21:41:49 +0800

    20260421-2141-更新index.js的cors

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(cors({
   origin: [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    // 'http://localhost:5173',
+    // 'http://127.0.0.1:5173',
     'http://192.168.0.187:5173' // 務必包含手機訪問的前端 IP
   ],
   credentials: true, // 允許跨網域傳遞 Cookie 或 Authorization Header
